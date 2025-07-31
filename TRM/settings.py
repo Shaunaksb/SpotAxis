a5d85b21840135b07c5a48703cdd290e768b3c63
Shaunaksb <43343551+Shaunaksb@users.noreply.github.com>
2025-07-31 18:38:57 +0530

    fixed static file issue

--- TRM/settings.py
+++ TRM/settings.py
@@ -241,7 +241,7 @@ MEDIA_ROOT = os.getenv('media_root', os.path.join(PROJECT_PATH, 'media'))
 MEDIA_URL = os.getenv('media_url', 'http://spotaxis.com/media/')
 STATIC_ROOT = os.getenv('STATIC_ROOT') or os.path.join(PROJECT_PATH, 'staticfiles')
 STATICFILES_DIRS = [
-    os.getenv('STATIC_DIR') or os.path.join(PROJECT_PATH, 'TRM', 'static')
+    os.getenv('STATIC_DIR') or os.path.join(PROJECT_PATH, 'static')
 ]
 
 # Site URL
